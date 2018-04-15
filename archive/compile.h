e9d34470db57cbf0e9e529c669b2e5873cdb8037
Eve-ning <johnchangqi@hotmail.com>
2018-04-15 16:10:50 +0800

    Move to Archive

---
+++ archive/compile.h
@@ -0,0 +1,43 @@
+//#ifndef COMPILE_H
+//#define COMPILE_H
+
+//#include <QString>
+//#include <QtWidgets>
+
+//#include "defargs.h"
+//#include "convert.h"
+//#include "amber.h"
+//#include "ui_amber.h"
+//#include "amber_base_inc.h"
+
+//namespace COMPILE {
+
+//    //OMPILERS
+//    QString     OM_NN   (QTextBrowser *tb,
+//                         cOM_HO &OM_HO);
+
+//    QString     OM_LN   (QTextBrowser *tb,
+//                         cOM_HO &OM_HO);
+
+//    QString     OM_BPM  (QTextBrowser *tb,
+//                         cOM_TP &OM_TP);
+
+//    QString     OM_SV   (QTextBrowser *tb,
+//                         cOM_TP &OM_TP);
+
+////    QString     BASIC_HO(QTextBrowser *tb,
+////                         QString noOfKeys = DEFARGS::noOfKeys,
+////                         QString offset = DEFARGS::offset,
+////                         QString column = DEFARGS::column,
+////                         QString lnOffset = DEFARGS::lnOffset,
+////                         QString label = DEFARGS::hitObjectLabel);
+
+////    QString     BASIC_TP(QTextBrowser *tb,
+////                         QString offset = DEFARGS::offset,
+////                         QString value = DEFARGS::bpmValue,
+////                         QString timingPointType = DEFARGS::timingPointType,
+////                         QString label = DEFARGS::timingPointLabel);
+
+//}
+
+//#endif // COMPILE_H
---
+++ archive/convert.h
@@ -0,0 +1,53 @@
+//#ifndef CONVERT_H
+//#define CONVERT_H
+
+//#include <QtWidgets>
+//#include <QString>
+//#include <QStringList>
+
+//#include "check.h"
+//#include "compile.h"
+
+////amespace CONVERT {
+
+////    //ONVERTERS
+////    double          COLUMN_CODEtoVALUE  (QTextBrowser *tb,
+////                                         double columnCode,
+////                                         double noOfKeys);
+////    double          COLUMN_VALUEtoCODE  (QTextBrowser *tb,
+////                                         double columnValue,
+////                                         double noOfKeys);
+
+////    QList<double>   EHOtoOFFSETLIST     (QTextBrowser *tb, QString EHO);
+////    QList<int>      EHOtoCOLUMNLIST     (QTextBrowser *tb, QString EHO);
+
+////    QList<double>   HOtoOFFSETLIST      (QTextBrowser *tb, QStringList HOList);
+////    QList<int>      HOtoCOLUMNLIST      (QTextBrowser *tb, QStringList HOList, int keyCount);
+////    QStringList     HOtoEXTENSIONLIST   (QTextBrowser *tb, QStringList HOList);
+
+////    QList<double>   TPtoOFFSETLIST      (QTextBrowser *tb, QStringList TPList);
+////    QList<double>   TPtoVALUELIST       (QTextBrowser *tb, QStringList TPList);
+////    QList<double>   TPtoCODELIST        (QTextBrowser *tb, QStringList TPList);
+
+////    QList<double>   TPtoSVVALUELIST     (QTextBrowser *tb, QStringList TPList);
+////    QList<double>   TPtoBPMVALUELIST    (QTextBrowser *tb, QStringList TPList);
+////    QList<double>   TPtoSVCODELIST      (QTextBrowser *tb, QStringList TPList);
+////    QList<double>   TPtoBPMCODELIST     (QTextBrowser *tb, QStringList TPList);
+
+////    double          TP_BPMCODEtoVALUE   (QTextBrowser *tb, double TPCode);
+////    double          TP_BPMVALUEtoCODE   (QTextBrowser *tb, double TPValue);
+
+////    double          TP_SVCODEtoVALUE    (QTextBrowser *tb, double TPCode);
+////    double          TP_SVVALUEtoCODE    (QTextBrowser *tb, double TPValue);
+
+////    QStringList     OMtoBASIC           (QTextBrowser *tb,
+////                                         bool acceptEHO = true,
+////                                         bool acceptHO = true,
+////                                         bool acceptTP = true,
+////                                         QStringList input = {},
+////                                         int noOfKeys = 4);
+
+////    QStringList     BASICtoOM           (QStringList input = {});
+////}
+
+//#endif // CONVERT_H
