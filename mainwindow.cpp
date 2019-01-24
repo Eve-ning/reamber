ad5472d769c38dc370ff15899d2ad76b0b8c9e21
Eve-ning <johnchangqi@hotmail.com>
2019-01-24 22:03:45 +0800

    Implement Copier
    Adjust hit_object_input requirements to editor_hit_object_input

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,7 +18,19 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_copier_generate_clicked()
 {
+    // We need the tp_v and ho_v for copier
+    // However, we don't actually need correct keys for hit_object
+
     timing_point_v tp_v;
     tp_v.load_raw_timing_point(ui->copier_tp->toPlainText().toStdString());
-    ui->copier_output->setPlainText(QString::fromStdString(tp_v.get_string_raw("\n")));
+
+    // We default keys to 0
+    hit_object_v ho_v;
+    ho_v.load_editor_hit_object(ui->copier_ho->toPlainText().toStdString());
+
+    // We only need the offset_v of ho_v to copy
+    ui->copier_output->setPlainText(
+                QString::fromStdString(
+                    lib_functions::create_copies(&tp_v, ho_v.get_offset_v(true),true,true)->get_string_raw("\n"))
+                );
 }
