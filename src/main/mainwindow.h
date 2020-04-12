3b949aa29c59eff75e1888b83bf5763e3eb43c5e
Evening <johnchangqi@hotmail.com>
2020-04-12 09:08:50 +0800

    Allow 0.01 SV

--- src/main/mainwindow.h
+++ src/main/mainwindow.h
@@ -85,9 +85,9 @@ private:
     Ui::MainWindow *ui;
 
     // Define "global" variables
-    constexpr static double SV_MIN = 0.1;
+    constexpr static double SV_MIN = 0.01;
     constexpr static double SV_MAX = 10.0;
-    constexpr static double BPM_MIN = 0.1;
+    constexpr static double BPM_MIN = 0.01;
     constexpr static double BPM_MAX = 10000000; // 10,000,000
 
     long double MATH_PI = 3.14159265358979323846l;
