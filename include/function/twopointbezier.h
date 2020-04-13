8131cee2c47f588fa0ff59b21c23e48598a90b63
Evening <johnchangqi@hotmail.com>
2020-04-13 16:34:30 +0800

    Implement mock TPB

--- include/function/twopointbezier.h
+++ include/function/twopointbezier.h
@@ -16,22 +16,33 @@ public:
     ~TwoPointBezier();
 
 protected:
-    QVector<QVector2D> createBezier(const QVector<double>& x,
-                                    const QVector<double>& y,
-                                    double start, double end, double interval);
-    QVector<QVector2D> createBezier(const QVector<double>& x,
-                                    const QVector<double>& y,
-                                    double interval);
-    QVector<QVector2D> createBezier(const QVector<QVector2D>& p,
-                                    double start, double end, double interval);
-    QVector<QVector2D> createBezier(const QVector<QVector2D>& p,
-                                    double interval);
+    QVector<QVector2D> createBezier(double start, double end);
+    QVector<QVector2D> createBezier();
 
+private slots:
+    void on_y1ms_valueChanged(double arg1);
+    void on_x2ms_valueChanged(double arg1);
+    void on_y2ms_valueChanged(double arg1);
+    void on_x3ms_valueChanged(double arg1);
+    void on_y3ms_valueChanged(double arg1);
+    void on_y4ms_valueChanged(double arg1);
+    void on_x4ms_valueChanged(double arg1);
+    void on_x2p_valueChanged(double arg1);
+    void on_x3p_valueChanged(double arg1);
+    void on_interval_valueChanged(int arg1);
+
+private:
+    bool live() const;
+    void updatePlot();
     void setCustomPlot(const QVector<double>& x,
                        const QVector<double>& y);
     void setCustomPlot(const QVector<QVector2D>& p);
-private:
+
+    void initP();
+    QVector<QVector2D> p;
+    int interval;
     Ui::TwoPointBezier *ui;
+
 };
 
 #endif // TWOPOINTBEZIER_H
--- src/function/twopointbezier.cpp
+++ src/function/twopointbezier.cpp
@@ -1,22 +1,18 @@
 #include "include/function/twopointbezier.h"
 #include "ui_twopointbezier.h"
 #include "depend/qcustomplot.h"
-#include <QDebug>
 #include <QVector2D>
 #include <QtGlobal>
 
 TwoPointBezier::TwoPointBezier(QWidget *parent) :
     QWidget(parent),
+    p(QVector<QVector2D>(4)),
+    interval(1000.0),
     ui(new Ui::TwoPointBezier)
 {
     ui->setupUi(this);
     ui->customplot->addGraph();
-    auto vec = createBezier(QVector<double>({0, 1, 9, 10}),
-                            QVector<double>({0, 9, 1, 10}),
-                            0.01);
-    ui->customplot->xAxis->setRange(0, 20);
-    ui->customplot->yAxis->setRange(0, 20);
-    setCustomPlot(vec);
+    initP();
 }
 
 TwoPointBezier::~TwoPointBezier()
@@ -24,113 +20,94 @@ TwoPointBezier::~TwoPointBezier()
     delete ui;
 }
 
-QVector<QVector2D> TwoPointBezier::createBezier(const QVector<double> &x, const QVector<double> &y, double start, double end, double interval)
-{
-    // general def
-    if (x.size() != y.size()){
-        qDebug() << "x and y size must match";
-        return QVector<QVector2D>({});
-    }
 
-    int size = x.size();
-    double sumX = 0.0;
-    double sumY = 0.0;
-    double t = 0.0;
-    int step = 0;
+
+QVector<QVector2D> TwoPointBezier::createBezier(double start, double end) {
+    int size = p.size();
+    double t = start;
 
     auto factorial = [](int a) -> int {
         int sum = 1;
         for (int i = 2; i <= a; i ++) sum *= i;
-        return sum; // int to double for safer conversion
+        return sum;
     };
 
     QVector<QVector2D> out;
+    std::sort(p.begin(), p.end(), [](QVector2D a, QVector2D b){ return a.x() < b.x(); });
 
     // API to include ends
-    while (t < 1.0) {
-        t = (interval * step) / (end - start);
-
-        sumX = 0.0;
-        sumY = 0.0;
-        for (int p = 0; p < size; p ++) {
-            int coeff = factorial(size - 1) / (factorial(p) * factorial(size - p - 1));
-            sumX += coeff * pow(1.0 - t, size - p - 1) * pow(t, p)* x[p];
-            sumY += coeff * pow(1.0 - t, size - p - 1) * pow(t, p)* y[p];
+    for (int step = 1; t < 1.0; step++) {
+        double sumX = 0.0;
+        double sumY = 0.0;
+        for (int n = 0; n < size; n ++) {
+            int coeff = factorial(size - 1) / (factorial(n) * factorial(size - n - 1));
+            sumX += coeff * pow(1.0 - t, size - n - 1) * pow(t, n) * double(p[n].x());
+            sumY += coeff * pow(1.0 - t, size - n - 1) * pow(t, n) * double(p[n].y());
         }
         out.push_back(QVector2D(float(sumX), float(sumY)));
-        step++;
+        t = (interval * step) / (end - start);
     }
-
+    out.push_back(QVector2D(float(end), float(p.last().y())));
 
     return out;
 }
-QVector<QVector2D> TwoPointBezier::createBezier(const QVector<double> &x, const QVector<double> &y, double interval)
-{
-    return createBezier(x, y,
-                        *std::min_element(x.begin(), x.end()),
-                        *std::max_element(x.begin(), x.end()),
-                        interval);
+
+QVector<QVector2D> TwoPointBezier::createBezier() {
+    // Automatically sets start and end
+    QVector<double> x = QVector<double>();
+    for (const auto & i : p) x.push_back(double(i.x()));
+    return createBezier(*std::min_element(x.begin(), x.end()),
+                        *std::max_element(x.begin(), x.end()));
 }
 
-QVector<QVector2D> TwoPointBezier::createBezier(const QVector<QVector2D> &p, double start, double end, double interval){
-    QVector<double> x;
-    QVector<double> y;
-
-    for (auto i : p) {
-        x.push_back(double(i.x()));
-        y.push_back(double(i.y()));
-    }
-
-    return createBezier(x, y, start, end, interval);
+bool TwoPointBezier::live() const {
+    return ui->check_live->isChecked();
 }
 
-QVector<QVector2D> TwoPointBezier::createBezier(const QVector<QVector2D> &p, double interval){
-    QVector<double> x;
-    QVector<double> y;
-
-    for (auto i : p) {
-        x.push_back(double(i.x()));
-        y.push_back(double(i.y()));
-    }
-
-    return createBezier(x, y,
-                        *std::min_element(x.begin(), x.end()),
-                        *std::max_element(x.begin(), x.end()),
-                        interval);
-}
-
-void TwoPointBezier::setCustomPlot(const QVector<double> &x, const QVector<double> &y) {
+void TwoPointBezier::updatePlot() {
+    if (!live()) return;
     auto customplot = ui->customplot;
+    auto vec = createBezier();
+    QVector<double> x = QVector<double>();
+    QVector<double> y = QVector<double>();
+    for (auto i : vec){
+        x.push_back(double(i.x()));
+        y.push_back(double(i.y()));
+    }
     customplot->graph(0)->setData(x, y, false);
+    auto xMinMax = std::minmax_element(x.begin(), x.end());
+    auto yMinMax = std::minmax_element(y.begin(), y.end());
+    customplot->xAxis->setRange(*xMinMax.first * 0.9, *xMinMax.second * 1.1);
+    customplot->yAxis->setRange(*yMinMax.first * 0.9, *yMinMax.second * 1.1);
     customplot->replot();
 }
 
-void TwoPointBezier::setCustomPlot(const QVector<QVector2D> &p) {
-    QVector<double> x;
-    QVector<double> y;
-
-    for (auto i : p) {
-        x.push_back(double(i.x()));
-        y.push_back(double(i.y()));
-    }
-
-    return setCustomPlot(x, y);
+void TwoPointBezier::initP()
+{
+    p[0] = QVector2D(float(ui->x1ms->value()), float(ui->y1ms->value()));
+    p[1] = QVector2D(float(ui->x2ms->value()), float(ui->y2ms->value()));
+    p[2] = QVector2D(float(ui->x3ms->value()), float(ui->y3ms->value()));
+    p[3] = QVector2D(float(ui->x4ms->value()), float(ui->y4ms->value()));
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void TwoPointBezier::on_y1ms_valueChanged(double arg1){ p[0].setY(float(arg1)); updatePlot(); }
+void TwoPointBezier::on_x2ms_valueChanged(double arg1){ p[1].setX(float(arg1)); updatePlot(); }
+void TwoPointBezier::on_y2ms_valueChanged(double arg1){ p[1].setY(float(arg1)); updatePlot(); }
+void TwoPointBezier::on_x3ms_valueChanged(double arg1){ p[2].setX(float(arg1)); updatePlot(); }
+void TwoPointBezier::on_y3ms_valueChanged(double arg1){ p[2].setY(float(arg1)); updatePlot(); }
+void TwoPointBezier::on_x4ms_valueChanged(double arg1){ p[3].setX(float(arg1)); updatePlot(); }
+void TwoPointBezier::on_y4ms_valueChanged(double arg1){ p[3].setY(float(arg1)); updatePlot(); }
+void TwoPointBezier::on_x2p_valueChanged(double arg1){
+    auto xms = float(arg1 / 100.0) * (p[3].x() - p[0].x()) + p[0].x();
+    p[1].setX(xms);
+    ui->x2ms->setValue(double(xms));
+    updatePlot();
+}
+void TwoPointBezier::on_x3p_valueChanged(double arg1){
+    auto xms = float(arg1 / 100.0) * (p[3].x() - p[0].x()) + p[0].x();
+    p[2].setX(xms);
+    ui->x3ms->setValue(double(xms));
+    updatePlot();
+}
+void TwoPointBezier::on_interval_valueChanged(int arg1){ interval = arg1; updatePlot(); }
 
