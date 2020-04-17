55cbe2983330c58f437fff26a2728569db9cf3f2
Evening <johnchangqi@hotmail.com>
2020-04-17 14:48:48 +0800

    Convert Stutter Case

--- src/function/stutter.cpp
+++ src/function/stutter.cpp
@@ -16,14 +16,14 @@ Stutter::~Stutter()
 }
 
 
-void Stutter::on_initsv_vs_valueChanged(int value) {
-    ui->initsv_val->setText(QString::number(value/VS_TO_VAL));
+void Stutter::on_initSvVs_valueChanged(int value) {
+    ui->initSvLabel->setText(QString::number(value/VS_TO_VAL));
 }
-void Stutter::on_initbpm_vs_valueChanged(int value) {
-    ui->initbpm_val->setText(QString::number(value/VS_TO_VAL));
+void Stutter::on_initBpmSlider_valueChanged(int value) {
+    ui->initBpmLabel->setText(QString::number(value/VS_TO_VAL));
 }
-void Stutter::on_threshold_vs_valueChanged(int value) {
-    ui->threshold_val->setText(QString::number(value/VS_TO_VAL));
+void Stutter::on_thresholdSlider_valueChanged(int value) {
+    ui->thresholdLabel->setText(QString::number(value/VS_TO_VAL));
     stutterLimitUpdate();
 }
 void Stutter::on_generate_clicked() {
@@ -38,29 +38,29 @@ void Stutter::on_generate_clicked() {
     TimingPointV tpV;
 
     // Depends on which radio is checked, we generate a different output
-    if (ui->type_sv->isChecked())
+    if (ui->svRadio->isChecked())
         tpV = algorithm::stutterRel(
                     hoV.getOffsetV(true),
-                    ui->initsv_val->text().toDouble(),
-                    ui->threshold_val->text().toDouble(),
+                    ui->initSvLabel->text().toDouble(),
+                    ui->thresholdLabel->text().toDouble(),
                     ui->avesv->value(),
                     false, true,
-                    ui->skip_last->isChecked());
-    else if (ui->type_bpm->isChecked())
+                    ui->skipLastCheck->isChecked());
+    else if (ui->bpmRadio->isChecked())
         tpV = algorithm::stutterRel(
                     hoV.getOffsetV(true),
-                    ui->initbpm_val->text().toDouble(),
-                    ui->threshold_val->text().toDouble(),
+                    ui->initBpmLabel->text().toDouble(),
+                    ui->thresholdLabel->text().toDouble(),
                     ui->avebpm->value(),
                     true, true,
-                    ui->skip_last->isChecked());
+                    ui->skipLastCheck->isChecked());
 
 
 
     ui->output->setPlainText(tpV.getStringRaw("\n"));
 
 }
-void Stutter::on_preset_nft_clicked() { // Normalized Front Teleport
+void Stutter::on_NormFrontTelButton_clicked() { // Normalized Front Teleport
     HitObjectV hoV;
     hoV.loadEditor(ui->input->toPlainText());
 
@@ -80,11 +80,11 @@ void Stutter::on_preset_nft_clicked() { // Normalized Front Teleport
     // We cannot directly omit since we need to stutter swap here
     tpV = algorithm::stutterSwap(tpV);
 
-    if (ui->skip_last->isChecked()) tpV.popBack();
+    if (ui->skipLastCheck->isChecked()) tpV.popBack();
 
     ui->output->setPlainText(tpV.getStringRaw("\n"));
 }
-void Stutter::on_preset_nbt_clicked() { // Normalized Back Teleport
+void Stutter::on_NormBackTelButton_clicked() { // Normalized Back Teleport
     HitObjectV hoV;
     hoV.loadEditor(ui->input->toPlainText());
 
@@ -99,11 +99,11 @@ void Stutter::on_preset_nbt_clicked() { // Normalized Back Teleport
                 true,
                 false,
                 true,
-                ui->skip_last->isChecked());
+                ui->skipLastCheck->isChecked());
 
     ui->output->setPlainText(tpV.getStringRaw("\n"));
 }
-void Stutter::on_preset_mft_clicked() { // Max Front Teleport
+void Stutter::on_MaxFronTelButton_clicked() { // Max Front Teleport
     HitObjectV hoV;
     hoV.loadEditor(ui->input->toPlainText());
 
@@ -133,7 +133,7 @@ void Stutter::on_preset_mft_clicked() { // Max Front Teleport
                     true,
                     true).getStringRaw("\n"));
 }
-void Stutter::on_preset_mbt_clicked() { // Max Back Teleport
+void Stutter::on_MaxBackTelButton_clicked() { // Max Back Teleport
     HitObjectV hoV;
     hoV.loadEditor(ui->input->toPlainText());
 
@@ -168,43 +168,43 @@ void Stutter::on_preset_mbt_clicked() { // Max Back Teleport
 void Stutter::on_avebpm_valueChanged(double) { stutterLimitUpdate(); }
 void Stutter::on_avesv_valueChanged(double) { stutterLimitUpdate(); }
 void Stutter::stutterLimitUpdate() {
-    if (ui->type_sv->isChecked()){
+    if (ui->svRadio->isChecked()){
         // We limit the initial SV values
         QVector<double> initLim = algorithm::stutterRelInitLimits(
-                    ui->threshold_val->text().toDouble(),
+                    ui->thresholdLabel->text().toDouble(),
                     ui->avesv->text().toDouble(), SV_MIN, SV_MAX);
 
         // If the lower limit is lower than SV_MIN we curb the setMinimum
         if (initLim[0] >= SV_MIN)
-            ui->initsv_vs->setMinimum(int(initLim[0] * VS_TO_VAL));
+            ui->initSvVs->setMinimum(int(initLim[0] * VS_TO_VAL));
         else
-            ui->initsv_vs->setMinimum(int(SV_MIN * VS_TO_VAL));
+            ui->initSvVs->setMinimum(int(SV_MIN * VS_TO_VAL));
 
 
         // If the upper limit is higher than SV_MAX we curb the setMaximum
         if (initLim[1] <= SV_MAX)
-            ui->initsv_vs->setMaximum(int(initLim[1] * VS_TO_VAL));
+            ui->initSvVs->setMaximum(int(initLim[1] * VS_TO_VAL));
         else
-            ui->initsv_vs->setMaximum(int(SV_MAX * VS_TO_VAL));
+            ui->initSvVs->setMaximum(int(SV_MAX * VS_TO_VAL));
 
-    } else if (ui->type_bpm->isChecked()) {
+    } else if (ui->bpmRadio->isChecked()) {
         // We limit the initial BPM values
         QVector<double> initLim = algorithm::stutterRelInitLimits(
-                    ui->threshold_val->text().toDouble(),
+                    ui->thresholdLabel->text().toDouble(),
                     ui->avebpm->text().toDouble(), BPM_MIN, BPM_MAX);
 
         // If the lower limit is higher than BPM_MIN we curb the setMinimum
         if (initLim[0] >= BPM_MIN) {
-            ui->initbpm_vs->setMinimum(int(initLim[0] * VS_TO_VAL));
+            ui->initBpmSlider->setMinimum(int(initLim[0] * VS_TO_VAL));
         } else {
-            ui->initbpm_vs->setMinimum(int(BPM_MIN * VS_TO_VAL));
+            ui->initBpmSlider->setMinimum(int(BPM_MIN * VS_TO_VAL));
         }
 
         // If the upper limit is lower than BPM_MAX we curb the setMaximum
         if (initLim[1] <= BPM_MAX) {
-            ui->initbpm_vs->setMaximum(int(initLim[1] * VS_TO_VAL));
+            ui->initBpmSlider->setMaximum(int(initLim[1] * VS_TO_VAL));
         } else {
-            ui->initbpm_vs->setMaximum(int(BPM_MAX * VS_TO_VAL));
+            ui->initBpmSlider->setMaximum(int(BPM_MAX * VS_TO_VAL));
         }
     }
 }
