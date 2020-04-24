43ae35476edb700ea342c49d6046c9456c149cc0
Evening <johnchangqi@hotmail.com>
2020-04-24 12:09:24 +0800

    Explicit double notation

--- include/common.h
+++ include/common.h
@@ -23,12 +23,12 @@ public:
     constexpr static double SV_STEPSIZE = 0.1;
 
     constexpr static double BPM_MIN = 0.01;
-    constexpr static double BPM_MAX = 10000000; // 10,000,000
+    constexpr static double BPM_MAX = 10000000.0; // 10,000,000
     constexpr static double BPM_DEFAULT = 120.0;
     constexpr static double BPM_STEPSIZE = 1.0;
 
     constexpr static double OFFSET_MIN = 0.0;
-    constexpr static double OFFSET_MAX = 100000000; // 100,000,000
+    constexpr static double OFFSET_MAX = 100000000.0; // 100,000,000
     constexpr static double OFFSET_DEFAULT = 0.0;
     constexpr static double OFFSET_INTERVAL_DEFAULT = 100.0;
 
